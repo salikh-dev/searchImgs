0b32139528c3efaef9d34f6c491cc9663d84197a
Bekzodbek <97502548+Bekzodbek2006@users.noreply.github.com>
2022-08-03 16:28:32 +0500

    commit

--- dictionary/models.py
+++ dictionary/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import AbstractUser
 
 
 class MyUser(AbstractUser):
+    
     img = models.ImageField(null=True, blank=True)
     bio = models.CharField(max_length=70, null=True, blank=True)
     
