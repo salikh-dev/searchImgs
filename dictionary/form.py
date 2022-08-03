a542331fccfaedaaaa614251b1d4c175765538d0
Bekzodbek <97502548+Bekzodbek2006@users.noreply.github.com>
2022-08-03 18:07:47 +0500

    commit

--- dictionary/form.py
+++ dictionary/form.py
@@ -1,7 +1,7 @@
 from dataclasses import fields
 from django.contrib.auth.views import get_user_model
 from django import forms
-from django.contrib.auth.forms import UserCreationForm, UsernameField, UserChangeForm
+from django.contrib.auth.forms import UserCreationForm, UsernameField, UserChangeForm, SetPasswordForm
 from .models import *
 
 User = get_user_model()
@@ -14,7 +14,19 @@ class SignUp(UserCreationForm):
         field_classes = {"username": UsernameField}
 
 
-class User_change(UserChangeForm):
-    class Meta:
-        model = User
-        fields = ("__all__")
+# class User_change(UserChangeForm):
+#     class Meta:
+#         model = User
+#         fields = ("__all__")
+
+class User_change(SetPasswordForm):
+        old_password = forms.CharField(
+        label=("Old password"),
+        strip=True,
+        widget=forms.PasswordInput(
+            attrs={"autocomplete": "current-password"}
+        ),
+    )
+        class Meta:
+            model= User
+            fields=("old_password", "new_password1", "new_password2")
