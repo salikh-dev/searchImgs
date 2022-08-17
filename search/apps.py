8034cf9e4af0fa92171f4cbf581e97a34b701a16
Bekzodbek <97502548+Bekzodbek2006@users.noreply.github.com>
2022-08-17 15:24:59 +0500

    add signal.py

--- search/apps.py
+++ search/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class SearchConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'search'
+
+    def ready(self):
+        import search.signal
--- search/signal.py
+++ search/signal.py
@@ -7,7 +7,7 @@ User = get_user_model()
 
 @receiver(post_save, sender=User)
 
-def create_profile(sender, instance, **kwargs):
+def create_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(cust_user=instance)
 
